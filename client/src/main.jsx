6eb9522abf1b4364c3fa93dac22e9ff6ebe37c02
hamzaamir-design <hamzaamir9733@gmail.com>
2025-10-05 16:18:06 +0500

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { store } from './redux/store.js';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store} >
-    <App />
-  </Provider>,
-)
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={<div>Loading persisted data...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  userReducer  from './user/userSlice.js';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+// Combine all reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// Persist config
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store
 export const store = configureStore({
-    reducer: {
-        user: userReducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
     }),
 });
+
+// Create persistor
+export const persistor = persistStore(store);
