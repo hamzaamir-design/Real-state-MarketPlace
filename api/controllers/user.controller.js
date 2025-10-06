d9eee892ac614d207019b3ce5044e12b0139e0db
hamzaamir-design <hamzaamir9733@gmail.com>
2025-10-06 06:15:30 +0500

    create user update api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,20 +1,36 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
 
 export const updateUser = async (req, res, next) => {
+ 
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(403, "You are not allowed to update this user!"));
+  }
+
   try {
+   
+    let updatedFields = {
+      username: req.body.username,
+      email: req.body.email,
+      avatar: req.body.avatar,
+    };
+
+    if (req.body.password) {
+      const hashedPassword = bcryptjs.hashSync(req.body.password, 10);
+      updatedFields.password = hashedPassword;
+    }
+
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
-      {
-        $set: {
-          username: req.body.username,
-          email: req.body.email,
-          password: req.body.password ? req.body.password : undefined,
-          avatar: req.body.avatar,
-        },
-      },
+      { $set: updatedFields },
       { new: true }
     );
+
+    if (!updatedUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
   } catch (err) {
@@ -22,9 +38,6 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-
 export const test = (req, res) => {
-    res.json({
-        "message": "Hello World"
-    });
+  res.json({ message: "Hello World" });
 };
