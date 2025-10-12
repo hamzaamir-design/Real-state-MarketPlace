61f8571ce9b7b8f99c123687cde366afb8c49d79
hamzaamir-design <hamzaamir9733@gmail.com>
2025-10-12 23:06:46 +0500

    complete home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sultan Estate</h1>
+      <p className='mb-4 text-slate-700'>
+        Sultan Estate is a trusted and rapidly growing real estate agency dedicated to helping clients buy, sell, and rent premium properties across Pakistan. 
+        With a strong focus on transparency, reliability, and customer satisfaction, we aim to make every real estate transaction a seamless experience.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our mission is to empower our clients with expert guidance, accurate market insights, and personalized service. 
+        Whether you're investing in your first home, selling your property, or seeking a rental opportunity, Sultan Estate provides the professional support and honest advice you need to make confident decisions.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Backed by a team of experienced real estate professionals, Sultan Estate combines market expertise with integrity and innovation. 
+        We believe real estate isn’t just about property—it’s about people, trust, and building lasting relationships. 
+        At Sultan Estate, your goals are our priority, and your success is our success.
+      </p>
+    </div>
   )
 }
